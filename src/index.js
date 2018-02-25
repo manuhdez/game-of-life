623a639fcc60f3f46f2fe07770efddd3f9c77e33
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-02-25 16:03:00 +0000

    created/ Grid component

--- src/index.js
+++ src/index.js
@@ -15,8 +15,13 @@ class Grid extends React.Component {
 class Main extends React.Component {
     constructor(){
         super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
         this.state = {
-            generation: 0
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
         }
     }
 
