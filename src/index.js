ab966dee56a846ffcc2d87d710fe98bfbd1a722d
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-02-25 16:29:30 +0000

    added/ initial seed grid functionality

--- src/index.js
+++ src/index.js
@@ -63,6 +63,32 @@ class Main extends React.Component {
         }
     }
 
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        })
+    }
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        })
+    }
+
+    componentDidMount() {
+        this.seed();
+    }
+
     render() {
         return(
             <div>
@@ -77,6 +103,10 @@ class Main extends React.Component {
             </div>
         )
     }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
